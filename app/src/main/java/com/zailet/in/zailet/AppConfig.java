67bd51bc37c1941a5ee4e4989979c64f3a0fa396
simran <simranpreetkaurkhalsa13@gmail.com>
2017-05-18 19:37:43 +0530

    authentication done

---
+++ app/src/main/java/com/zailet/in/zailet/AppConfig.java
@@ -0,0 +1,13 @@
+package com.zailet.in.zailet;
+
+/**
+ * Created by simran on 5/14/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.43.219/zailet/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.43.219/zailet/register.php";
+}
